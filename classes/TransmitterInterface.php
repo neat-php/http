5b6a0b81a3fc5d55e8a0b7b6cc57366dd17cd93a
Bauke van der Woude <bvanderwoude@shopbox.nl>
2019-08-01 11:11:38 +0200

    Added doc blocks to the transmitter interface

--- classes/TransmitterInterface.php
+++ classes/TransmitterInterface.php
@@ -6,15 +6,40 @@ use Neat\Http\Response\Redirect;
 
 interface TransmitterInterface
 {
+    /**
+     * @param string $html
+     * @return Response
+     */
     public function html(string $html): Response;
 
+    /**
+     * @param \JsonSerializable|array|object $body
+     * @return Response
+     */
     public function json($body): Response;
 
+    /**
+     * @return Redirect
+     */
     public function redirect(): Redirect;
 
+    /**
+     * @param resource $resource
+     * @param string   $name
+     * @param string   $mimeType
+     * @param bool     $attachment
+     * @return Response
+     */
     public function download($resource, string $name, string $mimeType, bool $attachment = true): Response;
 
+    /**
+     * @return Response
+     */
     public function response(): Response;
 
+    /**
+     * @param Response $response
+     * @return void
+     */
     public function send(Response $response);
 }
