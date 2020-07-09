2b2d323d6ef52963c66213df1882173fd19b6d8f
Wouter van Campen <wvancampen@ritel.nl>
2020-07-09 09:53:39 +0200

    RouteNotFoundException return 404 instead of 500.

--- classes/Exception/RouteNotFoundException.php
+++ classes/Exception/RouteNotFoundException.php
@@ -2,14 +2,16 @@
 
 namespace Neat\Http\Exception;
 
-use RuntimeException;
-
 /**
  * Route not found exception
  *
  * The requested resource could not be found but may be available in the future.
  * Subsequent requests by the client are permissible
  */
-class RouteNotFoundException extends RuntimeException
+class RouteNotFoundException extends StatusException
 {
+    public function __construct(string $reason = null, \Throwable $previous = null)
+    {
+        parent::__construct(404, $reason, $previous);
+    }
 }
